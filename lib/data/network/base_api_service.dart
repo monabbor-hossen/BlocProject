7c2f5f7d2be5009b2c4dde471957e937e51d4d7c
md-jihan <monabborhossen@gmail.com>
2026-01-07 00:04:19 +0600

    update

---
+++ lib/data/network/base_api_service.dart
@@ -0,0 +1,7 @@
+
+
+abstract class BaseApiService {
+  Future<dynamic> getApi(String url);
+  Future<dynamic> postApi(String url, var data);
+  Future<dynamic> deleteApi(String url);
+}
---
+++ lib/data/network/network_service_api.dart
@@ -0,0 +1,80 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:blocc/data/exceptions/app_exceptions.dart';
+import 'package:blocc/data/network/base_api_service.dart';
+import 'package:http/http.dart' as http;
+class NetworkServiceApi implements BaseApiService{
+  @override
+  Future<dynamic> deleteApi(String url) async{
+
+    dynamic jsonResponse;
+try{
+final response = await http.delete(Uri.parse(url)).timeout(const Duration(seconds: 50));
+jsonResponse = returnResponse(response);
+if(response.statusCode == 200){
+
+}
+}on SocketException{
+  throw NoInternetException('');
+}on TimeoutException{
+  throw FetchDataException('Time out try again');
+}
+
+  }
+  @override
+  Future<dynamic> getApi(String url) async{
+
+    dynamic jsonResponse;
+try{
+final response = await http.get(Uri.parse(url)).timeout(const Duration(seconds: 50));
+jsonResponse = returnResponse(response);
+if(response.statusCode == 200){
+
+}
+}on SocketException{
+  throw NoInternetException('');
+}on TimeoutException{
+  throw FetchDataException('Time out try again');
+}
+
+  }
+
+  @override
+  Future<dynamic> postApi(String url, data) async{
+
+    dynamic jsonResponse;
+    try{
+      final response = await http.post(Uri.parse(url),body: data).timeout(const Duration(seconds: 50));
+      jsonResponse = returnResponse(response);
+      if(response.statusCode == 200){
+
+      }
+    }on SocketException{
+      throw NoInternetException('');
+    }on TimeoutException{
+      throw FetchDataException('Time out try again');
+    }
+
+  }
+
+  dynamic returnResponse(http.Response response){
+    switch(response){
+      case 200:
+        dynamic jsonResponse =jsonDecode(response.body);
+        return jsonResponse;
+      case 400:
+        dynamic jsonResponse =jsonDecode(response.body);
+        return jsonResponse;
+      case 401:
+        throw UnauthorisedException();
+      case 500:
+        throw FetchDataException('Error communication with server.' + response.statusCode.toString());
+      default:
+        throw UnauthorisedException();
+    }
+  }
+
+
+}
